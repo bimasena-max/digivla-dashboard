8faa7e7369e065f5994552d58b792ef5dea24135
bimasena-max <bimasenayudhaprawira@gmail.com>
2026-06-02 10:41:22 +0700

    fix auth handler

--- api/auth.js
+++ api/auth.js
@@ -1,17 +1,21 @@
-const USERNAME = 'digivla'
-const PASSWORD = 'Digivla2026'
 const fs = require('fs')
 const path = require('path')
 
-export default function handler(req, res) {
+const USERNAME = 'digivla'
+const PASSWORD = 'Digivla2026'
+
+module.exports = function handler(req, res) {
   const authHeader = req.headers['authorization']
 
   if (authHeader) {
     const base64 = authHeader.split(' ')[1]
-    const [user, pass] = Buffer.from(base64, 'base64').toString().split(':')
+    const decoded = Buffer.from(base64, 'base64').toString()
+    const [user, pass] = decoded.split(':')
+
     if (user === USERNAME && pass === PASSWORD) {
-      const html = fs.readFileSync(path.join(process.cwd(), 'index.html'), 'utf8')
-      return res.setHeader('Content-Type', 'text/html').send(html)
+      const html = fs.readFileSync(path.join(process.cwd(), 'dashboard.html'), 'utf8')
+      res.setHeader('Content-Type', 'text/html')
+      return res.send(html)
     }
   }
 
